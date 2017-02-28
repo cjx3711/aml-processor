713913327b1172cd92b3726c4b1dbe40135475b0
Jia Xun <chaijiaxun@gmail.com>
2017-03-01 03:10:11 +0800

    WIP Aligner

--- pairedFastqProc.py
+++ pairedFastqProc.py
@@ -1,11 +1,57 @@
-from Bio.pairwise2 import *
+# from Bio.pairwise2 import *
 from j3xUtils import *
 from concurrent.futures import *
 
 numThreads = 12
 
 def alignUnpaired(left, right):
-    return align.localms(left, right, 1, -1, -10, -0.1, one_alignment_only = True)
+    print(left)
+    print(right)
+
+    # Slide the logical way
+    highestScore = -99999
+    finalOverlap = 0
+    for overlap in range(5, len(left)):
+        finalOverlap = overlap
+        score = 0
+        for i in zip(left[-overlap:], right[:overlap]):
+            if i[0] == 'N' or i[1] == 'N':
+                continue
+            elif i[0] != i[1]:
+                score = score - 5
+            elif i[0] == i[1]:
+                score = score + 1
+            else:
+                print("Hello?? " + item)
+        # print(score)
+        if ( score > highestScore ):
+            highestScore = score
+        if ( score > 10 ):
+            break
+
+    if overlap <= 10:
+        for overlap in range(5, len(left)):
+            finalOverlap = overlap
+            score = 0
+            for i in zip(left[:overlap], right[-overlap:]):
+                if i[0] == 'N' or i[1] == 'N':
+                    continue
+                elif i[0] != i[1]:
+                    score = score - 5
+                elif i[0] == i[1]:
+                    score = score + 1
+                else:
+                    print("Hello?? " + item)
+            # print(score)
+            if ( score > highestScore ):
+                highestScore = score
+            if ( score > 10 ):
+                break
+
+    print(finalOverlap)
+    print(left.ljust(len(left) - finalOverlap + len(left), '-'))
+    print(right.rjust(len(right) - finalOverlap + len(right), '-'))
+    # return align.localms(left, right, 1, -1, -10, -0.1, one_alignment_only = True)
 
 def pairAligned(aligned):
     baseScore = {"A": 1, "T": 1, "C": 1, "G": 1, "N": 0.5, "-": 0}
@@ -37,8 +83,17 @@ def release2Iters(iter1, iter2):
         if not i or not j: break
         yield i, j
 
-"""
-left = [[],"NTGGACTGATATGTGATTTATTCTTTCAACAGCCNNNNNNNGANCCANTGANNAACAAGCTCTCANNNNNNNNCTGAAGATAATGACTCACCNNGGGCCACATTNNAACATTGNAANNTNGCTGGGAGCCTGCACCAAGTCAGGTGGGCTC"]
-right = [[],"GAGAGTGGAGGATTNAAGNCTGATTGAACNNTTTTCACAACCANNNNNGTNCAGTGAAAATCCTCACTCCNGNTNNGTGAGCCNNNNNGNCNTGGTGCNGGCTCCCAGCANGNNNNNNNNGNNCNNNNGTGGCNNNNNNNNNNNNNNTATN"]
-print(alignAndMerge(left, right))
-"""
+left = [[],"NTGGACTGATATGTGATTTATTCTTTCAACAGCCNNNNNNNGANCCANTGANNAACAAGCTCTCANNNNNNNNCTGAAGATAATGACTCACCNNGGGCCACATTNNAACATTGNAANNTNGCTGGGAGCCTGCACCAAGTCAGGTGGGCTC\n"]
+right = [[],"GAGAGTGGAGGATTNAAGNCTGATTGAACNNTTTTCACAACCANNNNNGTNCAGTGAAAATCCTCACTCCNGNTNNGTGAGCCNNNNNGNCNTGGTGCNGGCTCCCAGCANGNNNNNNNNGNNCNNNNGTGGCNNNNNNNNNNNNNNTATN\n"]
+
+# print(alignAndMerge([left, right]))
+
+
+left = [[],"GGGGGGGGGGGGGGGGGGGGGGGGGGGGGGAAAAAAAAAAAAAAAA\n"]
+right = [[],"GGGGGGGGGGGGGGGGGGGGGGGGGGGGGTTTTTTTTTTTTTTTT\n"]
+
+
+left = [[],"AAAAAAAAAAAAAAAAGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG\n"]
+right = [[],"TTTTTTTTTTTTTTTTGGGGGGGGGGGGGGGGGGGGGGGGGGGGG\n"]
+
+print(alignAndMerge([left, right]))
