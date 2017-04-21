d3715dc720d343ea1480679c6de1b685f547c507
Jia Xun <chaijiaxun@gmail.com>
2017-04-22 00:29:49 +0800

    Created new hashing method, but super slow

--- milo/trie-test.py
+++ milo/trie-test.py
@@ -1,61 +1,94 @@
 import pygtrie
 import time
 
-kgramLength = 4
-spacing = 4
+kgramLength = 10
+spacing = 10
 
-indelTree = pygtrie.CharTrie()
+# indelTree = pygtrie.CharTrie()
 indelHash = {}
 
-
-reference = [
-"ATCGACCGTCGATGCACCATAG",
-"ATCCGTCGACGATGGTTGGATC",
-"TGCTAGCTTTAAACTGACGTTA",
-"GATACGTACCGTGGATGAGTGA",
-]
-# Read through each line of the reference file and create the kgrams
-lineno = 0
-for line in reference:
-    # Iterate through string and get kgrams
-    for i in range(0,len(line) - kgramLength, spacing):
-        kgram = line[i:i+kgramLength]
-        # Trie
-        if indelTree.has_key(kgram) == False:
-            indelTree[kgram] = []
-        indelTree[kgram].append((lineno,i))
-        if (kgram in indelHash) == False:
-            indelHash[kgram] = []
-        indelHash[kgram].append((lineno,i))
-    lineno += 1
-
-# Add each of the kgrams to the trie
-
-
-test = [
-    "ATCGACCGTCGATGCACCATAG",
-    "ATCCGTCGACGATGGTTGGATC",
-    "TGCTAGCTTTAAACTGACGTTA",
-    "GATACGTACCGTGGATGAGTGA",
-]
-
 start_time = time.time()
 
-for line in test:
-    matches = []
-    for i in range(0,len(line) - kgramLength + 1):
-        kgram = line[i:i+kgramLength]
-        if indelTree.has_key(kgram):
-            matches.append(indelTree[kgram])
+referenceCount = 0;
+with open("references/Manifest.csv", "r", newline = "") as references:
 
-print("--- %s seconds ---" % (time.time() - start_time))
+    # Read through each line of the reference file and create the kgrams
+    lineno = -1
+    for line in references:
+        if lineno >= 0:
+            referenceCount += 1
+            sequence = line.split(',')[2]
+            for i in range(0,len(sequence) - kgramLength, spacing):
+                kgram = sequence[i:i+kgramLength]
+                # # Trie
+                # if indelTree.has_key(kgram) == False:
+                #     indelTree[kgram] = []
+                # indelTree[kgram].append((lineno,i))
+                if (kgram in indelHash) == False:
+                    indelHash[kgram] = []
+                indelHash[kgram].append((lineno,i))
+        lineno += 1
 
-start_time = time.time()
+    # Add each of the kgrams to the trie
 
-for line in test:
-    for i in range(0,len(line) - kgramLength + 1):
-        kgram = line[i:i+kgramLength]
-        if kgram in indelHash:
-            matches.append(indelHash[kgram])
+
+# start_time = time.time()
+
+# with open("data/MINITEST_AD01_S1_L001PAIRED.j3x") as test:
+#     linecounter = -1
+#     for line in test:
+#         linecounter += 1
+#         if linecounter % 4 != 2:
+#             pass
+#
+#         matches = []
+#         for i in range(0,len(line) - kgramLength + 1):
+#             kgram = line[i:i+kgramLength]
+#             if indelTree.has_key(kgram):
+#                 matches.append(indelTree[kgram])
+
+
+# print("--- %s seconds ---" % (time.time() - start_time))
+
+
+badCount = 0
+total = 0
+
+with open("data/MINITEST_AD01_S1_L001PAIRED.j3x") as test:
+    linecounter = -1
+    for line in test:
+        linecounter += 1
+        if linecounter % 4 != 1:
+            continue
+
+        total += 1
+        # if linecounter > 2000:
+        #     break
+
+        matches = []
+        matchCounts = [[0,x] for x in range(referenceCount)]
+        
+        i = 0
+        while i < len(line) - kgramLength + 1:
+        # for i in range(0,len(line) - kgramLength + 1):
+            kgram = line[i:i+kgramLength]
+            if kgram in indelHash:
+                currentMatches = indelHash[kgram]
+                i += kgramLength
+                for match in currentMatches:
+                    matchCounts[match[0]][0] += 1
+                matches.extend(currentMatches)
+            i += 1
+        matchCounts.sort(reverse = True)
+        if ( matchCounts[1][0] / matchCounts[0][0] > 0.5 ):
+            badCount += 1
+            # print(str(matchCounts[0]) + str(matchCounts[1]))
+        #
+
+        # print(matchCounts)
+        # print(matches)
+
+print(badCount)
+print(total)
 
 print("--- %s seconds ---" % (time.time() - start_time))
