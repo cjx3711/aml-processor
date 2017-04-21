cbb72d2b4c913780d5fe55b6d9fb1445952715f0
Jia Xun <chaijiaxun@gmail.com>
2017-04-22 01:36:02 +0800

    Optimised hash processing

--- milo/trie-test.py
+++ milo/trie-test.py
@@ -1,13 +1,14 @@
 import pygtrie
 import time
 
-kgramLength = 10
-spacing = 10
+kgramLength = 15
+spacing = 15
 
 # indelTree = pygtrie.CharTrie()
 indelHash = {}
 
 start_time = time.time()
+
 
 referenceCount = 0;
 with open("references/Manifest.csv", "r", newline = "") as references:
@@ -52,8 +53,18 @@ with open("references/Manifest.csv", "r", newline = "") as references:
 
 
 badCount = 0
-total = 0
+ummCount = 0
 
+total = 0
+# matchCounts = [0] * referenceCount
+matchCounts = [[0,x] for x in range(referenceCount)]
+
+def resetMatchCounts(matchCounts):
+    for mc in matchCounts:
+        mc[0] = 0
+        mc[1] = i
+
+# with open("data/AD01_S1_L001PAIRED.j3x") as test:
 with open("data/MINITEST_AD01_S1_L001PAIRED.j3x") as test:
     linecounter = -1
     for line in test:
@@ -62,32 +73,35 @@ with open("data/MINITEST_AD01_S1_L001PAIRED.j3x") as test:
             continue
 
         total += 1
-        # if linecounter > 2000:
-        #     break
 
         matches = []
-        matchCounts = [[0,x] for x in range(referenceCount)]
-        
+        resetMatchCounts(matchCounts)
         i = 0
         while i < len(line) - kgramLength + 1:
-        # for i in range(0,len(line) - kgramLength + 1):
             kgram = line[i:i+kgramLength]
             if kgram in indelHash:
                 currentMatches = indelHash[kgram]
-                i += kgramLength
+                i += spacing - 1
                 for match in currentMatches:
                     matchCounts[match[0]][0] += 1
                 matches.extend(currentMatches)
             i += 1
         matchCounts.sort(reverse = True)
-        if ( matchCounts[1][0] / matchCounts[0][0] > 0.5 ):
+        # largest1, largest2 = getLargestTwo(matchCounts)
+        secondPercent = 0 # Percentage of the second largest
+        if ( matchCounts[0][0] != 0 ):
+            secondPercent = matchCounts[1][0] / matchCounts[0][0]
+
+        if (secondPercent > 0.6 ):
             badCount += 1
+        elif (secondPercent > 0.3 ):
+            ummCount += 1
             # print(str(matchCounts[0]) + str(matchCounts[1]))
-        #
+
 
         # print(matchCounts)
         # print(matches)
-
+print(ummCount)
 print(badCount)
 print(total)
 
