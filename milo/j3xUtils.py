662aefaebdf2d3473e00146d14a697d762bf91b0
Jia Xun <chaijiaxun@gmail.com>
2017-05-22 16:49:01 +0800

    WIP on hashing algo

--- milo/j3xUtils.py
+++ milo/j3xUtils.py
@@ -29,3 +29,38 @@ def fastqToJ3X(sequenceData):
     qualityScores = "".join(simplifyQuality(qualityDict[x]) for x in sequenceData[3][:-1])
     baseSeq = "".join("_" if x == "N" else x for x in sequenceData[1][:-1])
     return sequenceIdentifier + "\n" + baseSeq + "\n" + qualityScores + "\n" + "\n"
+    
+
+def getVal(hash, key):
+    if key in hash:
+        return hash[key]
+    return 0
+    
+def quickDistance(sequence1, sequence2):
+    hash1 = quickDistance(sequence1)
+    hash2 = quickDistance(sequence2)
+    dist = 0
+    dist = dist + abs(len(hash1) - len(hash2))
+    for i in range(min(len(hash1), len(hash2))):
+        h1 = hash1[i]
+        h2 = hash1[i]
+        dist = abs(getVal(h1, 'A'), getVal(h2, 'A')) + abs(getVal(h1, 'T'), getVal(h2, 'T')) + abs(getVal(h1, 'C'), getVal(h2, 'C')) + abs(getVal(h1, 'G'), getVal(h2, 'G'))
+    
+    return dist
+    
+def quickHash(sequence):
+    hash = []
+    for i in range(len(a) // 4):
+        partHash = {}
+        seq = sequence[i * 4: (i+1) * 4]
+        for c in seq:
+            if c in partHash:
+                partHash[c] = partHash[c] + 1
+            else:
+                partHash[c] = 1
+        hash.append(partHash) 
+    return hash
+    
+a = "ATCGCTACG"
+b = "ATCGCTACG"
+print(quickDistance(a,b))
