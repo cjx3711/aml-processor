b422d79551080a8ea9d6128db36d8f4ce905b938
Jia Xun <chaijiaxun@gmail.com>
2017-08-10 14:00:23 +0800

    Added potential optimisation features

--- milo/main-mutationid.py
+++ milo/main-mutationid.py
@@ -3,12 +3,14 @@ from concurrent.futures import *
 from MutationFinder import *
 import json
 from pprint import pprint
+import time
 
 inDir = "data/Processed/"
 outDir = "data/Processed/"
 
 numThreads = 12
-mutationFinder = MutationFinder()
+groupsOf = 4
+mutationFinder = MutationFinder(groupsOf)
 
 def run():
     minMutationCount = 2
@@ -29,9 +31,11 @@ def run():
         
         for filenames in filenameArray:
             pairedFile, mutationFile = readFilenames(filenames)
-
+            start = time.time()
             # Don't understand the __name__ thing, but it's required according to SO
             if __name__ ==  "__main__": mutationID(pairedFile, mutationFile, inDir, outDir, minMutationCount)
+            end = time.time()
+            print("Took {0}s".format(end - start))
 
 def readFilenames(filenames):
     pairedFile = mutationFile = ''
@@ -47,11 +51,13 @@ def mutationID(pairedFile, mutationFile, inDir, outDir, minMutationCount):
             print("Crunching {0}".format(pairedFile))
             
             # Creates iterators which deliver the 4 lines of each FASTQ read as a zip (ID, Sequence, Blank, Quality)
-            inFileIter = grouper(inFile, 4)
+            # 4 * groupsOf because we wanna send more reads at a time
+            inFileIter = grouper(inFile, 4 * groupsOf)
             with ProcessPoolExecutor(numThreads) as processManager:
                 # Calls alignAndMerge(FASTQ1's (ID, Sequence, Blank, Quality), FASTQ2's (ID, Sequence, Blank, Quality))
-                for ampliconID, mutationHash in processManager.map(mutationFinder.identifyMutations, inFileIter, chunksize = 250):
-                    mutationFinder.putMutationMap(ampliconID, mutationHash)
+                
+                for i in processManager.map(mutationFinder.identifyMutations, inFileIter, chunksize = 250):
+                    pass
                 
                 print("Dumping {0}".format(mutationFile))
                 mutationList = mutationFinder.extractHighestOccuringMutations(minMutationCount)
