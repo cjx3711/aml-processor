f81863ed4ad0a3b156e1e795264df0405c9b9dff
Jia Xun <chaijiaxun@gmail.com>
2017-08-10 21:39:34 +0800

    Added progress bar to ampliconid

--- milo/main-ampliconid.py
+++ milo/main-ampliconid.py
@@ -1,13 +1,21 @@
-from manifestExtraction import *
+from manifestExtraction import grouper
 from unpairedFastqProc import *
 from ReadPairer import *
+from tqdm import tqdm
+from multiprocessing import cpu_count
+
+import os
 import json
+import time
+
 
 inDir = "data/1-raw/"
 outDir = "data/2-paired/"
 
-numThreads = 12
+numThreads = cpu_count()
+chunksize = 250
 readPairer = ReadPairer(probabilistic = False)
+bytesPerRead = 350 # Estimated
 
 def run():
     with open('files.json') as file_list_file:    
@@ -40,18 +48,30 @@ def readFilenames(filenames):
     
 def pairToJ3X(fq1, fq2, paired, inDir, outDir):
     with open(inDir + fq1) as fq1File, open(inDir + fq2) as fq2File:
-        outfile = outDir + paired
-        print("Crunching {0} and {1}".format(fq1, fq2))
+        filesize1 = os.path.getsize(inDir + fq1)
+        filesize2 = os.path.getsize(inDir + fq1)
+        filesize = (filesize1 + filesize2) / 2
+        estimatedReads = int(filesize / bytesPerRead)
         
+        outfile = outDir + paired
         with open(outfile, "w+", newline = "") as outFile:
+            print("{0} Crunching {1} and {2}".format(time.strftime('%X %d %b %Y'), fq1, fq2))
+            start = time.time()
+            
             # Creates iterators which deliver the 4 lines of each FASTQ read as a zip (ID, Sequence, Blank, Quality)
             fq1Iter, fq2Iter = grouper(fq1File, 4), grouper(fq2File, 4)
-            with ProcessPoolExecutor(numThreads) as processManager:
-                # Calls alignAndMerge(FASTQ1's (ID, Sequence, Blank, Quality), FASTQ2's (ID, Sequence, Blank, Quality))
-                for x in processManager.map(readPairer.alignAndMerge, fq1Iter, fq2Iter, chunksize = 250):
-                    outFile.write(x)
+            # pool = Pool(numThreads)
+            processManager = ProcessPoolExecutor(numThreads)
+            with tqdm(total=estimatedReads) as pbar:
+                result = processManager.map(readPairer.alignAndMerge, fq1Iter, fq2Iter, chunksize = chunksize)
+                for i, data in tqdm(enumerate(result)):
+                    outFile.write(data)
                     outFile.write("\n\n")
+                    pbar.update()
                 outFile.close()
-                print("Dumped {0}".format(paired))
+            pbar.close()
+
+            print("{0} Dumped {1}".format(time.strftime('%X %d %b %Y'), paired))
+            print("Took {0}s".format(time.time() - start))
 
 run()
