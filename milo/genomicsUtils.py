ee8571f5ebaa69ec8496e76f8bb047e818535b86
CJX3711 <chaijiaxun@gmail.com>
2017-08-15 14:44:57 +0800

    WIP on new distance

--- milo/genomicsUtils.py
+++ milo/genomicsUtils.py
@@ -61,3 +61,49 @@ def getPhredErrorDict():
     0 to 1
     """
     return {x : pow(10, -y/10) for x, y in zip(phredList, range(42, -1, -1))}
+    
+def simpleDistance(a, b):
+    """
+    Checks the distance between two strings.
+    Outputs 0, 1 or 2. If it's 2, it means it's more than 2
+    """
+    if ( a == b ):
+        return 0
+    if ( abs(len(a) - len(b)) >= 2 ):
+        return 2
+    cursorA = 0
+    cursorB = 0
+    differences = 0
+    
+    
+    while ( cursorA < len(a) or cursorB < len(b) ):        
+        print("{0}/{1} {2}/{3}".format(cursorA, len(a), cursorB, len(b)))
+        if ( a[cursorA] == b[cursorB] ):
+            cursorA += 1
+            cursorB += 1
+            continue;
+        else: # Check the +1 for each
+            differences += 1
+            if ( cursorA >= len(a) - 1 or cursorB >= len(b) - 1 ):
+                break;
+            if ( a[cursorA + 1] == b[cursorB + 1] ): # Possible sub
+                print("Sub")
+                cursorA += 1
+                cursorB += 1
+            elif ( a[cursorA] == b[cursorB + 1] ): # Possible ins
+                print("Ins")
+                cursorB += 1
+            elif ( a[cursorA + 1 ] == b[cursorB] ): # possible del
+                print("Del")
+                cursorA += 1
+            else:
+                print("Uhh")
+                cursorA += 1
+                cursorB += 1
+    
+    if ( differences >= 2 ):
+        differences = 2
+    print ("Differences {0}".format(differences))
+    return differences
+    
+simpleDistance("ABAAA", "AAAA")
