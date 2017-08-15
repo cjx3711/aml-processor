d6b3b3e497b6c7c050451976114c94816dbfca83
CJX3711 <chaijiaxun@gmail.com>
2017-08-16 01:45:53 +0800

    Added config and stats file

--- milo/ReadCompressor.py
+++ milo/ReadCompressor.py
@@ -1,9 +1,20 @@
 from genomicsUtils import simpleDistance
+import json
 
 class ReadCompressor:
     def __init__(self):
         self.ampliconCountDict = {}
         self.totalReads = 0
+        
+        with open('config.json') as config_file: 
+            config_data = json.load(config_file)
+            if ( 'compressorAllowedThreshold' in config_data ):
+                self.compressorAllowedThreshold = config_data['compressorAllowedThreshold']
+            if ( 'compressorMergeThreshold' in config_data ):
+                self.compressorMergeThreshold = config_data['compressorMergeThreshold']
+            
+        if ( self.compressorAllowedThreshold <= self.compressorMergeThreshold ):
+            print("ERROR: compressorAllowedThreshold must be greater than self.compressorMergeThreshold")
         
     def putPairedRead(self, data):
         self.totalReads += 1
@@ -18,16 +29,21 @@ class ReadCompressor:
     
     def getDataList(self):
         readTupleList = list(self.ampliconCountDict.items())
-        onesList = [x for x in readTupleList if x[1][0] <= 1 ]
-        filteredTupleList = [x for x in readTupleList if x[1][0] > 2]
+        onesList = [x for x in readTupleList if x[1][0] <= self.compressorMergeThreshold ]
+        filteredTupleList = [x for x in readTupleList if x[1][0] >= self.compressorAllowedThreshold]
         filteredTupleList.sort(key=lambda tup: -tup[1][0])
-        
+        matchedOnes = 0
+        totalOnes = 0
         for ones in onesList:
+            onesOccurence = ones[1][0]
+            totalOnes += onesOccurence
             for tuples in filteredTupleList:
                 dist = simpleDistance(ones[0], tuples[0])
                 if ( dist < 2 ):
                     # print("Match\n{0}\n{1}".format(ones[0], tuples[0]))
-                    tuples[1][0] += ones[1][0] # Add to total count
-                    tuples[1][1] += ones[1][0] # Add to close match count
+                    tuples[1][0] += onesOccurence # Add to total count
+                    tuples[1][1] += onesOccurence # Add to close match count
+                    matchedOnes += onesOccurence
                     break
-        return filteredTupleList
+                
+        return filteredTupleList, totalOnes, matchedOnes        
