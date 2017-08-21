493c8f42cb245c8613590e47caa32b2bec239bf2
CJX3711 <chaijiaxun@gmail.com>
2017-08-22 03:28:47 +0800

    Added translocation to j4x files

--- milo/MutationFinder.py
+++ milo/MutationFinder.py
@@ -1,8 +1,9 @@
 from j4xUtils import *
+from TranslocatedBlockMatcher import *
 
 class MutationFinder:
-        
     def __init__(self):
+        self.translocatedBlockMatcher = TranslocatedBlockMatcher()
         pass
         
     def reinit(self):
@@ -13,6 +14,7 @@ class MutationFinder:
         #    ...
         # ]
         self.ampliconMutationHashList = []
+        self.ampliconTranslocationList = []
         
         self.referenceCount = 0
         self.ampliconRefs = []
@@ -28,9 +30,9 @@ class MutationFinder:
                     
                     # ampliconRefs format
                     # [
-                    #    [ SEQUENCE, COORDINATES, COUNT ]
+                    #    [ SEQUENCE, COORDINATES, READ_COUNT, MUTATION_COUNT, TRANSLOCATION_COUNT ]
                     # ]
-                    self.ampliconRefs.append([sequence, coordinates, 0]);
+                    self.ampliconRefs.append([sequence, coordinates, 0, 0, 0]);
                 lineno += 1
     
     def getReferenceAmpliconArray(self):
@@ -46,30 +48,71 @@ class MutationFinder:
         if ( ampliconID == 0 ):
             return
         key = "{0} {1}".format(ampliconID, mutationHash)
-        referenceAmplicon = self.getReferenceAmplicon(ampliconID)
-        referenceAmplicon[2] += readCount # Increment the count
+        
+        # Increment the mutation count
+        self.getReferenceAmplicon(ampliconID)[2] += readCount
         
         if ( len(mutationHash) == 0 ):
             return
+            
+        self.getReferenceAmplicon(ampliconID)[3] += readCount
         self.ampliconMutationHashList.append((key, readCount))
         # if ( key not in self.ampliconMutationHashDict ):
         #     self.ampliconMutationHashDict[key] = 0
         # self.ampliconMutationHashDict[key] += 1
     
+    def putTranslocationHash(self, ampID1, ampID2, matchingBlocks, readCount):
+        if ( ampID1 == 0 or ampID2 == 0 ):
+            return
+        
+        self.getReferenceAmplicon(ampID2)[2] += readCount
+        self.getReferenceAmplicon(ampID1)[2] += readCount
+        
+        if ( len(matchingBlocks) == 0 ):
+            return
+            
+        self.getReferenceAmplicon(ampID1)[4] += readCount
+        self.getReferenceAmplicon(ampID2)[4] += readCount
+        self.ampliconTranslocationList.append(('{0} {1}'.format(ampID1, ampID2), readCount))
+        
     def extractHighestOccuringMutations(self, minOccurences):
         filteredTupleList = [x for x in self.ampliconMutationHashList if x[1] >= minOccurences]
         filteredTupleList.sort(key=lambda tup: -tup[1])
         return filteredTupleList
         
-    def identifyMutations(self, data):
+    def extractHighestOccuringTranslocations(self, minOccurences):
+        filteredTupleList = [x for x in self.ampliconTranslocationList if x[1] >= minOccurences]
+        filteredTupleList.sort(key=lambda tup: -tup[1])
+        return filteredTupleList
+        
+    def identifyTranslocations(self, data):
         iddataParts = data[0].split(', ')
-        # Ignore translocations
-        if ( iddataParts[0].startswith('TL:') ):
-            return None, None, None, None
+        amplicons = iddataParts[0][3:].split('/')
+        ampID1 = int(amplicons[0])
+        ampID2 = int(amplicons[1])
+        if ampID1 == 0 or ampID2 == 0:
+            return 'T', None
+            
+        readCount = int(float(iddataParts[3].strip()[2:]))
+        sequenceData = data[1][:-1]
+        
+        refAmplicon1 = self.getReferenceAmplicon(ampID1)[0]
+        refAmplicon2 = self.getReferenceAmplicon(ampID2)[0]
+        
+        matchingBlocks = self.translocatedBlockMatcher.findTranslocatedMatchingBlocks(sequenceData, refAmplicon1, refAmplicon2)
+        
+        return 'T', ampID1, ampID2, matchingBlocks, readCount
+        
+    def identifyMutations(self, data):
+        # Handle translocations
+        if ( data[0].startswith('TL:') ):
+            return self.identifyTranslocations(data)
+            
+        iddataParts = data[0].split(', ')
             
         ampliconID = int(float(iddataParts[0][3:]))
         if ( ampliconID == 0 ):
-            return None, None, None, None
+            return 'M', None, None, None, None
         readCount = int(float(iddataParts[3].strip()[2:]))
         sequenceData = data[1][:-1]
         referenceAmplicon = self.getReferenceAmplicon(ampliconID)
@@ -78,4 +121,4 @@ class MutationFinder:
         referenceCoordinate = referenceAmplicon[1]
         mutationHash = mutationIDAsHash(referenceSequence, sequenceData)
         
-        return ampliconID, mutationHash, referenceCoordinate, readCount
+        return 'M', ampliconID, mutationHash, referenceCoordinate, readCount
